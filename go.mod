9921a5c3e8b7ccd45c29c61be93eedb6645368a6
Aqsha-Perdana <deliveryaqsha@gmail.com>
2025-07-13 11:12:04 +0700

    update go dan say hello

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Aqsha-Perdana/go_say_hello
+module github.com/Aqsha-Perdana/go_say_hello/v2
 
 go 1.24.4
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "say hello Aqsha"
+func SayHello(name string) string {
+	return "say hello, my name is " + name
 }
